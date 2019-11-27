eb2f8f29790a78e57a1ef2a53df568ee56f92689
dexterautonomy <chineduezeigbo2015@gmail.com>
2019-11-27 14:55:54 +0100

    hh

--- src/main/java/com/example/demo_two/controllers/HomeController.java
+++ src/main/java/com/example/demo_two/controllers/HomeController.java
@@ -1,12 +1,12 @@
 package com.example.demo_two.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController
 {
-    @RequestMapping("/home")
+    @GetMapping("/")
     public String getHomepage()
     {
         return "home";
